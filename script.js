c156521ca65c7776f39df626ce35b5faa2202d63
budds300 <budds300@gmail.com>
2021-05-03 21:13:21 +0300

    question 2

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const quizz{
-    quiz:"Who founded the python language"
+const quizz=[{
+    quiz:"Who founded the python language?",
+    a: "Guido Van Rossum",
+    b: "James Gosling",
+    c: "Dennis Ritchie",
+    d: "Ken Thomson",
+    correct:"a",
+},
+{
+    quiz: "How many stomach's does a cow have?",
+    a:"1",
+    b:"2",
+    c:"4",
+    d:"3",
+},
+{
+    quiz:"What is HTML in full?",
+    
 }
