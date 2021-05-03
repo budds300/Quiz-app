4916a1069dbd458581cdd7cf8ca16abb25987bfa
budds300 <budds300@gmail.com>
2021-05-03 21:17:33 +0300

    question 3

--- script.js
+++ script.js
@@ -15,5 +15,26 @@ const quizz=[{
 },
 {
     quiz:"What is HTML in full?",
-    
+    a: "Higher Transformative Machine Language",
+    b: "HyperText Markup Language",
+    c: "Javascript Notation",
+    d: "none of the above",
+    correct: "b",
+},
+{
+    quiz: "The answers below shows different technologies and frameworks and the organisations that created them.Which of the answers is false?",
+    a: "Python - FaceBook",
+    b: "Bootstrap - Twitter",
+    c: "Angular - Google",
+    d: "TypeScript - Microsoft",
+    correct: "a",
+},
+{
+    quiz: "Which programming language is the most used as of 2020?",
+    a: "Python",
+    b: "C",
+    c: "Java",
+    d: "JavaScript",
+    correct: "d",
 }
+]
